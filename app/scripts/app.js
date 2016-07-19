6e9816b0244df9fe0ece370605c04a907bbe4257
leonardo <leonardo.t.cardoso@hotmail.com>
2016-07-19 15:13:39 -0300

    por fazer

---
+++ app/scripts/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name routingQuizApp
+ * @description
+ * # routingQuizApp
+ *
+ * Main module of the application.
+ */
+
+angular
+  .module('routingQuizApp',[]);
+
+
+angular
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/instructions.html'
+      })
+      .state('cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      })
+      .state('blue-bricks', {
+        url: '/bricks/blue',
+        templateUrl: 'views/bricks.html',
+        controller: 'BlueBricksCtrl as brick'
+      })
+      .state('green-bricks', {
+        url: '/bricks/green',
+        templateUrl: 'views/bricks.html',
+        controller: 'GreenBricksCtrl as brick'
+      })
+      .state('red-bricks', {
+        url: '/bricks/red',
+        templateUrl: 'views/bricks.html',
+        controller: 'RedBricksCtrl as brick'
+      })
+      .state('red-bricks.cart', {
+        url: '/bricks/red/cart',
+        templateUrl: 'views/cart.html'
+      })
+      ;
+
+  }]);
+  
